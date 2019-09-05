782a7dcdca08e5274ad61562661d74e549a7177e
pali08 <p.mikulaj@gmail.com>
2019-09-05 22:35:42 +0200

    newest

--- CreateMolecule/MoleculeInteractorStyleAppendCH3.h
+++ CreateMolecule/MoleculeInteractorStyleAppendCH3.h
@@ -20,46 +20,48 @@ public:
   static MoleculeInteractorStyleAppendCH3* New();
   vtkTypeMacro(MoleculeInteractorStyleAppendCH3, vtkInteractorStyleTrackballCamera);
 
-  double VectorMagnitude(double x, double y, double z)
+double VectorMagnitude(double x, double y, double z)
+{
+  return sqrt(pow(x,2) + pow(y,2) + pow(z,2));
+}
+
+virtual void OnLeftButtonDown() override
+{
+  this->Interactor->GetPicker()->Pick(this->Interactor->GetEventPosition()[0], 
+                                      this->Interactor->GetEventPosition()[1], 
+                                      0,  // always zero.
+                                      this->Interactor->GetRenderWindow()->GetRenderers()->GetFirstRenderer());
+  
+  double* picked = this->Interactor->GetPicker()->GetPickPosition();
+  //TODO bonded atoms to current atom je potrebne niekde vyprazdnit 
+  std::vector<std::array<long long int, 3>> Bonds = molecule->GetAtomBonds();
+  long long int NumberOfAtoms = molecule->GetNumberOfAtoms();
+  long long int NumberOfBonds = molecule->GetNumberOfBonds();
+  for (long long int i=0; i<NumberOfAtoms; i++)
   {
-    return sqrt(pow(x,2) + pow(y,2) + pow(z,2));
-  }
-
-  virtual void OnLeftButtonDown() override
-  {
-    this->Interactor->GetPicker()->Pick(this->Interactor->GetEventPosition()[0], 
-                                        this->Interactor->GetEventPosition()[1], 
-                                        0,  // always zero.
-                                        this->Interactor->GetRenderWindow()->GetRenderers()->GetFirstRenderer());
-    double picked[3];
-    this->Interactor->GetPicker()->GetPickPosition(picked);
-
-    std::vector<std::array<long long int, 3>> Bonds = molecule->GetAtomBonds();
-
-    for (long long int i=0; i<molecule->GetNumberOfAtoms(); i++)
+    std::vector<long long int> BondedAtomsToCurrentAtom;
+    double AtomX = molecule->GetAtom(i).GetPosition().GetX();
+    double AtomY = molecule->GetAtom(i).GetPosition().GetY();
+    double AtomZ = molecule->GetAtom(i).GetPosition().GetZ();
+    double DistX = AtomX - picked[0];
+    double DistY = AtomY - picked[1];
+    double DistZ = AtomZ - picked[2];
+    for (long long int j = 0; j < NumberOfBonds; j++)
     {
-      std::vector<long long int> BondedAtomsToCurrentAtom;
-      double AtomX = molecule->GetAtom(i).GetPosition().GetX();
-      double AtomY = molecule->GetAtom(i).GetPosition().GetY();
-      double AtomZ = molecule->GetAtom(i).GetPosition().GetZ();
-      double DistX = AtomX - picked[0];
-      double DistY = AtomY - picked[1];
-      double DistZ = AtomZ - picked[2];
-      for (long long int j = 0; j < molecule->GetNumberOfBonds(); j++)
+      for (int k = 0; k < 2; k++) //first and second Id - for now we dont't check bond order
       {
-        for (int k = 0; k < 2; k++) //first and second Id - for now we dont't check bond order
-        {
-          if (Bonds[j][k] == i){ // we find that this atom has bonds
-            int FoundAtomId = k ^ 1; // if matched atom is on position [j][0] we need to get atom on position [j][1] and vice versa
-            BondedAtomsToCurrentAtom.push_back(FoundAtomId);
+        if (Bonds[j][k] == i)   // we find that this atom has bonds
+        { 
+          int FoundAtomFirstOrSecond = k ^ 1; // if matched atom is on position [j][0] we need to get atom on position [j][1] and vice versa
+          BondedAtomsToCurrentAtom.push_back(Bonds[j][FoundAtomFirstOrSecond]);
         }
       }
-
-      if (VectorMagnitude(DistX, DistY, DistZ) < 2.0) //need to play a little bit and relaize, how numbers work (different sizes of window etc)  
-      {
-        molecule->AppendAtom(12, GetNewAtomPosition(BondedAtomsToCurrentAtom, i));
-        break;
-      }
+    }
+    if (VectorMagnitude(DistX, DistY, DistZ) < 2.0) //need to play a little bit and relaize, how numbers work (different sizes of window etc)  
+    {
+      molecule->AppendAtom(12, GetNewAtomPosition(BondedAtomsToCurrentAtom, i));
+      molecule->AppendBond(molecule->GetAtom(i), molecule->GetAtom(NumberOfAtoms), 1);
+      break;
     }
   }
     // Forward events
@@ -107,6 +109,12 @@ public:
     @param BondedAtomsToCurrentAtom vector of atom Ids that are connected to current atom
     @return position of appended 
     */
+    //cout << AtomId;
+    for (int j = 0; j < BondedAtomsToCurrentAtom.size(); j++)
+    {
+      cout << BondedAtomsToCurrentAtom[j] << ',';
+    }
+    cout << endl;
     std::array<float, 3> ResultData = {0.0, 0.0, 0.0};
     if(BondedAtomsToCurrentAtom.size() == 1)
     {
@@ -117,6 +125,10 @@ public:
       for (int i = 0; i < 3; i++)
       {
         ResultData[i] = 2*CurrentAtomData[i] - BondedAtomData[i];
+        //cout << ResultData[i];
+        //cout << ',';
+        //ResultData[i] = 2 * BondedAtomData[i] - CurrentAtomData[i];
+        //ResultData[i] = ResultData[i] + 1.0;
       }
     }
     return ResultData;
